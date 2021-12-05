625d81e5323a8ba68cdff7b7c9796d466c42eff2
tempe-techie <95053628+tempe-techie@users.noreply.github.com>
2021-12-05 18:33:48 +0100

    home page with all NFT contracts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,50 @@
 <script setup lang="ts">
+import { onMounted, ref, watch } from "vue"
+import { useEthers } from 'vue-dapp'
+import useKuponFactory from "../hooks/useKuponFactory" 
+import NftCard from "../components/NftCard.vue"
 
+const { isActivated } = useEthers()
+const { contract: factoryContract } = useKuponFactory()
+
+// DATA
+const nftContractAddresses = ref()
+
+// ON CREATE
+onMounted(() => {
+  fetchNftAddresses()
+});
+
+// METHODS
+async function fetchNftAddresses() {
+  if (isActivated.value) {
+    const addrLength = await factoryContract().getNftAddressesLength()
+
+    let tempArray = []
+
+    for (let i = 0; i < addrLength; i++) {
+      let tempAddress = await factoryContract().getNftAddressByIndex(i)
+      tempArray.push(tempAddress)
+    }
+
+    nftContractAddresses.value = tempArray
+  }
+}
+
+// WATCHERS
+watch(isActivated, function () {
+  fetchNftAddresses()
+})
 </script>
 
 <template>
   <div class="text-center">
     <h1>Kupon Protocol</h1>
+
+    <div class="row mb-5">
+      <div class="col-4" v-for="nftAddress in nftContractAddresses">
+        <NftCard :address="nftAddress" />
+      </div>
+    </div>
   </div>
 </template>
